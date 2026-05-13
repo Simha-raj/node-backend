828a5b4ec8c50be2848c7a946ec45b77ecc4302e
Simha-raj <polireddyrajulanaidu123@gmail.com>
2026-05-13 17:55:57 +0530

    Fix casing of body-parser import

--- controller/control.js
+++ controller/control.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 //Mongodb connection
